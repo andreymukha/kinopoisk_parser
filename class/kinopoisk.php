1f4a055a57f963fbaa329209a392df3730e4b8e4
Andrey Mukha <andryfly7@gmail.com>
2015-07-17 02:27:28 +0300

    Перенёс содержимое методов, которые получали данные, в один инит метод, вызвал его в конструкторе. Спарсил список фильмов

--- class/kinopoisk.php
+++ class/kinopoisk.php
@@ -12,15 +12,20 @@ require_once $dir.'lib/phpQuery/phpQuery.php';
 
 class KP {
   private $artist_page;
-  public $search_name;
-  public $rus_name;
-  public $eng_name;
-  public $career;
-  public $height;
-  public $birthday;
-  public $place_of_birth;
-  public $genres;
-  public $family;
+
+  private $img;
+  private $search_name;
+  private $rus_name;
+  private $eng_name;
+  private $career;
+  private $height;
+  private $birthday;
+  private $place_of_birth;
+  private $genres;
+  private $family;
+  private $film_list;
+
+
 
   private function getContent($link){
     $result = system::getUrlContent(
@@ -44,51 +49,36 @@ class KP {
 
   private function getArtistPage($name){
     $search_page = self::getContent('http://www.kinopoisk.ru/index.php?first=no&what=&kp_query='.urlencode($name));
-    preg_match('!name\/([0-9]+)\/!', $search_page, $artist_id);
-    $artist_page = self::getContent('http://www.kinopoisk.ru/name/'.$artist_id[1]);
+    $artist_page_url = substr('http://www.kinopoisk.ru' . phpQuery::newDocument($search_page)->find('.most_wanted .name a')->attr('href'), 0, -5);
+    $artist_page = self::getContent($artist_page_url);
     return str_replace("charset=windows-1251", "charset=utf-8", $artist_page);
   }
 
    public function __construct($name){
     $this->search_name = $name;
     $this->artist_page = phpQuery::newDocument(self::getArtistPage($this->search_name), "text/html; charset=windows-1251");
-    $this->rus_name = self::_rusName();
-    $this->eng_name = self::_engName();
-    $this->career = self::_career();
-    $this->height = self::_height();
-    $this->birthday = self::_birthday();
-    $this->place_of_birth = self::_placeOfBirth();
-    $this->genres = self::_genres();
-    $this->family = self::_family();
+    $this->GetArtistInit();
   }
 
-  private function _rusName (){
-    $rus_name = $this->artist_page->find('div#headerPeople h1')->text();
-    return $rus_name;
-  }
+  public function GetArtistInit(){
+    $this->img = base64_encode(file_get_contents($this->artist_page->find('.film-img-box a img')->attr('src')));
 
-  private function  _engName(){
-    $eng_name = trim($this->artist_page->find('div#headerPeople span')->text());
-    return $eng_name;
-  }
+    $this->rus_name = $this->artist_page->find('div#headerPeople h1')->text();
 
-  private function _career(){
+    $this->eng_name = trim($this->artist_page->find('div#headerPeople span')->text());
+
     $careers = $this->artist_page->find('.info tr:contains(карьера) a');
     $career = array();
     foreach($careers as $part){
       $career[] = pq($part)->text();
     }
-    return $career;
-  }
+    $this->career = $career;
 
-  private function _height(){
-    $height = $this->artist_page->find('.info tr:contains(рост) span')->text();
-    return $height;
-  }
+    $this->height = $this->artist_page->find('.info tr:contains(рост) span')->text();
 
-  private function _birthday(){
     //todo разбить на отдельные поля
     $birthday = $this->artist_page->find('.info tr:contains(дата рождения)');
+
     $timestamp = array();
     foreach(pq($birthday)->find('a') as $cnt=>$date) {
       if($cnt == 1 or $cnt == 0){
@@ -103,34 +93,121 @@ class KP {
     }
     $year = $this->artist_page->find('.info tr:contains(дата рождения) td:not([class="type"])')->text();
     preg_match('![0-9]+ (года|лет)!', $year, $year);
-    $birthday = $timestamp[0].' '.$timestamp[1].' ('.$year[0].') - '.$zodiac;
-    return $birthday;
-  }
+    $this->birthday = $timestamp[0].' '.$timestamp[1].' ('.$year[0].') - '.$zodiac;
 
-  private function _placeOfBirth(){
     $places_of_birth = $this->artist_page->find('.info tr:contains(место рождения) a');
     $place_of_birth = array();
     foreach($places_of_birth as $part){
       $place_of_birth[] = pq($part)->text();
     }
-    return $place_of_birth;
-  }
+    $this->place_of_birth = $place_of_birth;
 
-  private function _genres(){
     $genres = $this->artist_page->find('.info tr:contains(жанры) a');
     $genre = array();
     foreach($genres as $part){
       $genre[] = pq($part)->text();
     }
-    return $genre;
-  }
+    $this->genres = $genre;
 
-  private function _family(){
     //todo поместить в многомерный массив
-    $family = $this->artist_page->find('.info tr:contains(супруг) td:last-child')->text();
-    return $family;
+    $this->family = $this->artist_page->find('.info tr:contains(супруг) td:last-child')->text();
+
+    $films = $this->artist_page->find('.specializationBox');
+    $film_list = array();
+    foreach($films as $film){
+      $headersAmplua = pq($film)->find('.headersAmplua span.txtWorks')->text();
+      foreach(pq($film)->find('.personPageItems .item') as $item){
+        $film_list[$headersAmplua][pq($item)->find('.name a')->text()] = array(
+          'url' => 'http://www.kinopoisk.ru' . pq($item)->find('.name a')->attr('href'),
+          'rating' => pq($item)->find('.rating a')->text(),
+          'role' => pq($item)->find('.role')->text(),
+        );
+      }
+    }
+    $this->film_list = $film_list;
   }
 
+  /**
+   * @return mixed
+   */
+  public function getSearchName() {
+    return $this->search_name;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getRusName() {
+    return $this->rus_name;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getEngName() {
+    return $this->eng_name;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getCareer() {
+    return $this->career;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getHeight() {
+    return $this->height;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getBirthday() {
+    return $this->birthday;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getPlaceOfBirth() {
+    return $this->place_of_birth;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getGenres() {
+    return $this->genres;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getFamily() {
+    return $this->family;
+  }
+
+  /**
+   * @param bool|TRUE $with_tag
+   * @return string
+   */
+  public function getImg($with_tag = true) {
+    if($with_tag){
+      return '<img alt="" src="data:image/jpg;base64,'.$this->img.'" />';
+    }else{
+      return $this->img;
+    }
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getFilmList() {
+    return $this->film_list;
+  }
 
   public function test(){
     echo self::getArtistPage($this->search_name);
